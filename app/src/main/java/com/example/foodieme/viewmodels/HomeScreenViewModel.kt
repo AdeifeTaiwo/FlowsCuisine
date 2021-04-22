5efe154f68a4705055d6c6e4023663cb87b23c08
Adeife Taiwo <adeifetaiwo50@gmail.com>
2021-04-22 09:49:30 +0100

    ...

--- app/src/main/java/com/example/foodieme/viewmodels/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/foodieme/viewmodels/HomeScreenViewModel.kt
@@ -2,15 +2,33 @@ package com.example.foodieme.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.foodieme.database.getDatabase
+import com.example.foodieme.domain.FlowsMenu
 import com.example.foodieme.repository.FlowsMenuRepository
 import kotlinx.coroutines.launch
 
 class HomeScreenViewModel(application: Application) : AndroidViewModel(application) {
 
+
+
+    //private val _foodList = MutableLiveData<List<FlowsMenu>>()
+
+
+
+
+
+    private var filter = FilterHolder()
+
+
+
     private val database = getDatabase(application)
     private val menuRepository = FlowsMenuRepository(database)
+
+
+    var foodList: LiveData<List<FlowsMenu>> = menuRepository.flowsMenu
 
     init{
         viewModelScope.launch {
@@ -18,6 +36,22 @@ class HomeScreenViewModel(application: Application) : AndroidViewModel(applicati
         }
     }
 
-    val flowsMenu = menuRepository.flowsMenu
+
     val popularFlowsMenu = menuRepository.popularFlowsMenu
+
+
+    fun onQueryChanged(filter: String?) : LiveData<List<FlowsMenu>>{
+       return menuRepository.getFlowMenuByCategory(filter)
+    }
+
+
+
+   
+
+
+
+
+
+
+
 }
